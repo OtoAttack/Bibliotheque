780d5f78d74095e47e492c4eb5c8afb56224a0c9
JJ <redon.jeanjacques@gmail.com>
2021-05-21 09:31:11 +0200

    Exercice 2

--- GestionBibli.js
+++ GestionBibli.js
@@ -5,4 +5,16 @@ var livre1 = {
     titre: "Le Pain",
     auteur: "JJ Redon"
 }
-console.log(livre1);
+var livre2 = {
+    référence: 2,
+    titre: "Le jus de fruit",
+    auteur: "JJ"
+}
+var livre3 = {
+    référence: 3,
+    titre: "Les smarties",
+    auteur: "Redon"
+}
+var livres = [livre1, livre2, livre3];
+var lenL = livres.length;
+console.log(livres);
